true gradient fix

sam_jax/models/pyramidnet.py
@@ -87,7 +87,8 @@ class BottleneckShakeDrop(nn.Module):
             alpha_max: float,
             beta_min: float,
             beta_max: float,
-            train: bool = True) -> jnp.ndarray:
+            train: bool = True,
+            true_gradient: bool = False) -> jnp.ndarray:
     """Implements the forward pass in the module.
 
     Args:
@@ -102,6 +103,8 @@ class BottleneckShakeDrop(nn.Module):
       beta_max: See paper.
       train: If False, will use the moving average for batch norm statistics.
         Else, will use statistics computed on the batch.
+      true_gradient: If true, the same mixing parameter will be used for the
+        forward and backward pass (see paper for more details).
 
     Returns:
       The output of the bottleneck block.
@@ -133,9 +136,10 @@ class BottleneckShakeDrop(nn.Module):
         name='1x1_conv_expand')
     y = utils.activation(y, apply_relu=False, train=train, name='bn_3')
 
-    if train:
+    if train and not self.is_initializing():
       y = utils.shake_drop_train(y, prob, alpha_min, alpha_max,
-                                 beta_min, beta_max)
+                                 beta_min, beta_max,
+                                 true_gradient=true_gradient)
     else:
       y = utils.shake_drop_eval(y, prob, alpha_min, alpha_max)
 
@@ -158,7 +162,8 @@ class PyramidNetShakeDrop(nn.Module):
             num_outputs: int,
             pyramid_alpha: int = 200,
             pyramid_depth: int = 272,
-            train: bool = True) -> jnp.ndarray:
+            train: bool = True,
+            true_gradient: bool = False) -> jnp.ndarray:
     """Implements the forward pass in the module.
 
     Args:
@@ -170,6 +175,8 @@ class PyramidNetShakeDrop(nn.Module):
       pyramid_depth: See paper.
       train: If False, will use the moving average for batch norm statistics.
         Else, will use statistics computed on the batch.
+      true_gradient: If true, the same mixing parameter will be used for the
+        forward and backward pass (see paper for more details).
 
     Returns:
       The output of the PyramidNet model, a tensor of shape
@@ -213,7 +220,8 @@ class PyramidNetShakeDrop(nn.Module):
           alpha_max,
           beta_min,
           beta_max,
-          train=train)
+          train=train,
+          true_gradient=true_gradient)
       layer_num += 1
 
     for block_i in range(blocks_per_group):
@@ -224,7 +232,8 @@ class PyramidNetShakeDrop(nn.Module):
                               ((2, 2) if block_i == 0 else (1, 1)),
                               layer_mask_prob,
                               alpha_min, alpha_max, beta_min, beta_max,
-                              train=train)
+                              train=train,
+                              true_gradient=true_gradient)
       layer_num += 1
 
     for block_i in range(blocks_per_group):
@@ -235,7 +244,8 @@ class PyramidNetShakeDrop(nn.Module):
                               ((2, 2) if block_i == 0 else (1, 1)),
                               layer_mask_prob,
                               alpha_min, alpha_max, beta_min, beta_max,
-                              train=train)
+                              train=train,
+                              true_gradient=true_gradient)
       layer_num += 1
 
     assert layer_num - 1 == total_blocks

sam_jax/models/utils.py
@@ -81,7 +81,8 @@ def dense_layer_init_fn(key: jnp.ndarray,
 
 def shake_shake_train(xa: jnp.ndarray,
                       xb: jnp.ndarray,
-                      rng: Optional[jnp.ndarray] = None) -> jnp.ndarray:
+                      rng: Optional[jnp.ndarray] = None,
+                      true_gradient: bool = False) -> jnp.ndarray:
   """Shake-shake regularization in training mode.
 
   Shake-shake regularization interpolates between inputs A and B
@@ -92,6 +93,8 @@ def shake_shake_train(xa: jnp.ndarray,
     xa: Input, branch A.
     xb: Input, branch B.
     rng: PRNG key.
+    true_gradient: If true, the same mixing parameter will be used for the
+      forward and backward pass (see paper for more details).
 
   Returns:
     Mix of input branches.
@@ -104,10 +107,12 @@ def shake_shake_train(xa: jnp.ndarray,
   # Draw different interpolation factors (gate) for forward and backward pass.
   gate_forward = jax.random.uniform(
       gate_forward_key, gate_shape, dtype=jnp.float32, minval=0.0, maxval=1.0)
+  x_forward = xa * gate_forward + xb * (1.0 - gate_forward)
+  if true_gradient:
+    return x_forward
   gate_backward = jax.random.uniform(
       gate_backward_key, gate_shape, dtype=jnp.float32, minval=0.0, maxval=1.0)
   # Compute interpolated x for forward and backward.
-  x_forward = xa * gate_forward + xb * (1.0 - gate_forward)
   x_backward = xa * gate_backward + xb * (1.0 - gate_backward)
   # Combine using stop_gradient.
   return x_backward + jax.lax.stop_gradient(x_forward - x_backward)
@@ -133,7 +138,8 @@ def shake_drop_train(x: jnp.ndarray,
                      alpha_max: float,
                      beta_min: float,
                      beta_max: float,
-                     rng: Optional[jnp.ndarray] = None) -> jnp.ndarray:
+                     rng: Optional[jnp.ndarray] = None,
+                     true_gradient: bool = False) -> jnp.ndarray:
   """ShakeDrop training pass.
 
   See https://arxiv.org/abs/1802.02375
@@ -146,6 +152,8 @@ def shake_drop_train(x: jnp.ndarray,
     beta_min: Beta range lower.
     beta_max: Beta range upper.
     rng: PRNG key (if `None`, uses `flax.nn.make_rng`).
+    true_gradient: If true, the same mixing parameter will be used for the
+      forward and backward pass (see paper for more details).
 
   Returns:
     The regularized tensor.
@@ -168,6 +176,8 @@ def shake_drop_train(x: jnp.ndarray,
       beta_key, rnd_shape, dtype=jnp.float32, minval=beta_min, maxval=beta_max)
   # See Eqn 6 in https://arxiv.org/abs/1802.02375.
   rand_forward = mask + alpha_values - mask * alpha_values
+  if true_gradient:
+    return x * rand_forward
   rand_backward = mask + beta_values - mask * beta_values
   return x * rand_backward + jax.lax.stop_gradient(
       x * rand_forward - x * rand_backward)

sam_jax/models/wide_resnet_shakeshake.py
@@ -107,7 +107,8 @@ class ShakeShakeBlock(nn.Module):
             x: jnp.ndarray,
             channels: int,
             strides: Tuple[int, int] = (1, 1),
-            train: bool = True) -> jnp.ndarray:
+            train: bool = True,
+            true_gradient: bool = False) -> jnp.ndarray:
     """Implements the forward pass in the module.
 
     Args:
@@ -117,6 +118,8 @@ class ShakeShakeBlock(nn.Module):
       strides: Strides for the pooling.
       train: If False, will use the moving average for batch norm statistics.
         Else, will use statistics computed on the batch.
+      true_gradient: If true, the same mixing parameter will be used for the
+        forward and backward pass (see paper for more details).
 
     Returns:
       The output of the resnet block. Will have shape
@@ -164,7 +167,7 @@ class ShakeShakeBlock(nn.Module):
     b = utils.activation(b, apply_relu=False, train=train, name='bn_b_2')
 
     if train and not self.is_initializing():
-      ab = utils.shake_shake_train(a, b)
+      ab = utils.shake_shake_train(a, b, true_gradient=true_gradient)
     else:
       ab = utils.shake_shake_eval(a, b)
 
@@ -182,7 +185,8 @@ class WideResnetShakeShakeGroup(nn.Module):
             blocks_per_group: int,
             channels: int,
             strides: Tuple[int, int] = (1, 1),
-            train: bool = True) -> jnp.ndarray:
+            train: bool = True,
+            true_gradient: bool = False) -> jnp.ndarray:
     """Implements the forward pass in the module.
 
     Args:
@@ -194,6 +198,8 @@ class WideResnetShakeShakeGroup(nn.Module):
       strides: Strides for the pooling.
       train: If False, will use the moving average for batch norm statistics.
         Else, will use statistics computed on the batch.
+      true_gradient: If true, the same mixing parameter will be used for the
+        forward and backward pass (see paper for more details).
 
     Returns:
       The output of the resnet block. Will have shape
@@ -205,7 +211,8 @@ class WideResnetShakeShakeGroup(nn.Module):
           x,
           channels,
           strides if i == 0 else (1, 1),
-          train=train)
+          train=train,
+          true_gradient=true_gradient)
     return x
 
 
@@ -217,7 +224,8 @@ class WideResnetShakeShake(nn.Module):
             blocks_per_group: int,
             channel_multiplier: int,
             num_outputs: int,
-            train: bool = True) -> jnp.ndarray:
+            train: bool = True,
+            true_gradient: bool = False) -> jnp.ndarray:
     """Implements a WideResnet with ShakeShake regularization module.
 
     Args:
@@ -231,6 +239,8 @@ class WideResnetShakeShake(nn.Module):
         for a classification problem).
       train: If False, will use the moving average for batch norm statistics.
         Else, will use statistics computed on the batch.
+      true_gradient: If true, the same mixing parameter will be used for the
+        forward and backward pass (see paper for more details).
 
     Returns:
       The output of the WideResnet with ShakeShake regularization, a tensor of
@@ -248,18 +258,21 @@ class WideResnetShakeShake(nn.Module):
         x,
         blocks_per_group,
         16 * channel_multiplier,
-        train=train)
+        train=train,
+        true_gradient=true_gradient)
     x = WideResnetShakeShakeGroup(
         x,
         blocks_per_group,
         32 * channel_multiplier, (2, 2),
-        train=train)
+        train=train,
+        true_gradient=true_gradient)
     x = WideResnetShakeShakeGroup(
         x,
         blocks_per_group,
         64 * channel_multiplier, (2, 2),
-        train=train)
+        train=train,
+        true_gradient=true_gradient)
     x = jax.nn.relu(x)
-    x = nn.avg_pool(x, (8, 8))
+    x = nn.avg_pool(x, x.shape[1:3])
     x = x.reshape((x.shape[0], -1))
     return  nn.Dense(x, num_outputs, kernel_init=utils.dense_layer_init_fn)

sam_jax/training_utils/flax_training.py
@@ -493,15 +493,22 @@ def train_step(
       a dictionary containing the training loss and error rate on the batch.
   """
 
-  def forward_and_loss(model: flax.nn.Model):
+  def forward_and_loss(model: flax.nn.Model, true_gradient: bool = False):
     """Returns the model's loss, updated state and predictions.
 
     Args:
       model: The model that we are training.
+      true_gradient: If true, the same mixing parameter will be used for the
+        forward and backward pass for the Shake Shake and Shake Drop
+        regularization (see papers for more details).
     """
     with flax.nn.stateful(state) as new_state:
       with flax.nn.stochastic(prng_key):
-        logits = model(batch['image'], train=True)
+        try:
+          logits = model(
+              batch['image'], train=True, true_gradient=true_gradient)
+        except TypeError:
+          logits = model(batch['image'], train=True)
     loss = cross_entropy_loss(logits, batch['label'])
     # We apply weight decay to all parameters, including bias and batch norm
     # parameters.
@@ -528,7 +535,7 @@ def train_step(
     """
     # compute gradient on the whole batch
     (_, (inner_state, _)), grad = jax.value_and_grad(
-        forward_and_loss, has_aux=True)(model)
+        lambda m: forward_and_loss(m, true_gradient=True), has_aux=True)(model)
     if FLAGS.sync_perturbations:
       if FLAGS.inner_group_size is None:
         grad = jax.lax.pmean(grad, 'batch')